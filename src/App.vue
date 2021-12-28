0ddb367f9870eca47ee9d60a2cc88c7eb08c1141
Vinicius Gandra <viniciusgandra2016@gmail.com>
2021-12-28 16:36:51 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,110 @@
 <template>
-<h1>Teste Repositório</h1>
+
+<div class="corpo">
+
+  <h1 class="centralizado">{{ titulo }}</h1>
+
+  <input type="search" class="filtro" @input= "filtro = $event.target.value" placeholder="filtre pelo título da foto">
+
+  <ul class="lista-fotos">
+
+    <li class="lista-fotos-item" v-for="foto in fotosComFiltro" :key="foto.id">
+
+      <meu-painel :titulo="foto.titulo">
+        <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+      </meu-painel>
+
+    </li>
+  </ul>
+  
+</div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
+
 export default {
-  
+
+  components: {
+
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
+
+  },
+
+  data() {
+
+    return {
+
+      titulo: 'Pesquisa',
+
+      fotos: [],
+
+      filtro: ''
+    
+    }
+  },
+
+  computed: {
+
+    fotosComFiltro() {
+      if (this.filtro) {
+
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+       
+        
+      } else {
+        
+        return this.fotos;
+        
+      }
+    }
+  },
+
+  created () {
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+    
+  }
 }
+
 </script>
 
 <style>
+
+.centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+    
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  
+  }
+
+  .imagem-responsiva {
+
+    width: 100%;
+
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
 
 </style>
